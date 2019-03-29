5aad14bef187365e9f369291f39bf28ac5c9a3d5
khairul-abdi <kabdi384@gmail.com>
2019-03-29 20:55:39 +0700

    first commit

---
+++ src/args.java
@@ -0,0 +1,9 @@
+public class args {
+    public static void main(String[] args) {
+        System.out.print("Argumen yang diberikan");
+
+        for (int i=0; i <args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+}
